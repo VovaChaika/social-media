84555e0dcd71dd6a20157b383bff33aa7c3460d3
VovaChaika <75807498+VovaChaika@users.noreply.github.com>
2022-07-11 09:59:27 +0300

    Updates

---
+++ precache-manifest.e9bc57d4548b37533f1c3dc2c094cd6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "617113865400784fd03e",
+    "url": "/social-media/static/css/main.fbaed6fc.chunk.css"
+  },
+  {
+    "revision": "617113865400784fd03e",
+    "url": "/social-media/static/js/main.61711386.chunk.js"
+  },
+  {
+    "revision": "fa164b1f42823202dcdc",
+    "url": "/social-media/static/js/1.fa164b1f.chunk.js"
+  },
+  {
+    "revision": "e538181a9d904afa707c",
+    "url": "/social-media/static/js/runtime~main.e538181a.js"
+  },
+  {
+    "revision": "540348f5cd31acf268d27b60e05a9442",
+    "url": "/social-media/index.html"
+  }
+];
