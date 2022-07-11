2a7e89186d7381e980e1c6b93b0992e12d7e8793
VovaChaika <75807498+VovaChaika@users.noreply.github.com>
2022-07-11 14:10:57 +0300

    Update Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,11 +6,8 @@ import noUserIcon from "../../assets/img/none-user-icon.png"
 let Users = (props) => {
     if (props.users.length === 0) {
         axios.defaults.withCredentials = true;
-        axios.defaults.headers = {
-            "API-KEY": "fc253b1c-0962-4c3c-bbdd-f6ced18294b8"
-        }
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
+        axios.get("https://jsonplaceholder.typicode.com/todos/1/users").then(response => {
+            props.setUsers(response.data)
         })
     }
 
@@ -20,7 +17,7 @@ let Users = (props) => {
                 u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small != null ? u.photos.small : noUserIcon} className={styles.userPhoto}/>
+                        <img src={noUserIcon} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -36,7 +33,7 @@ let Users = (props) => {
                 <span>
                     <span>
                         <div>{u.name}</div>
-                        <div>{u.status}</div>
+                        <div>{u.email}</div>
                     </span>
                 </span>
             </div>)
